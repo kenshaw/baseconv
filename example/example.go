22d519f8ff24f8408f7433727b02d23deeee6c6c
Kenneth Shaw <kenneth.shaw@knq.io>
2015-08-19 15:34:31 +0700

    Fixing example and associated README.md entry from last commit

--- example/example.go
+++ example/example.go
@@ -9,13 +9,14 @@ import (
 
 func main() {
 	valHex := "70b1d707eac2edf4c6389f440c7294b51fff57bb"
-	valDec := baseconv.DecodeHex(valHex)
-	val62 := baseconv.Convert(valHex, baseconv.DigitsHex, baseconv.Digits62)
-	val36 := baseconv.Encode36(val62, baseconv.Digits62)
+	valDec, _ := baseconv.DecodeHex(valHex)
+	val62, _ := baseconv.Convert(valHex, baseconv.DigitsHex, baseconv.Digits62)
+	val36, _ := baseconv.Encode36(val62, baseconv.Digits62)
 
 	fmt.Println("dec string: " + valDec)
 	fmt.Println("62 string:  " + val62)
 	fmt.Println("36 string:  " + val36)
 
-	fmt.Printf("dec and 36 values same: %t\n", valDec == baseconv.Decode36(val36, baseconv.DigitsDec))
+	conVal36, _ := baseconv.Decode36(val36, baseconv.DigitsDec)
+	fmt.Printf("dec and 36 values same: %t\n", valDec == conVal36)
 }
